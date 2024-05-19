446550bf6daf73b129e907dd1d96d3ccafcdaaf0
davit-aspire <davit@aspire.com>
2024-05-19 17:48:04 +0545

    navbar compeltion

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -17,16 +17,16 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="py-10 flex justify-between">
-      <h1 className="text-sm font-bold underline underline-offset-8 decoration-green-500 -rotate-2">
+    <nav className="py-10 flex justify-between items-center">
+      <h1 className="text-sm font-bold underline underline-offset-8 decoration-green-500 -rotate-6 hover:scale-150 transition-all">
         Davit・khanal • アイ
       </h1>
-      <div>
+      <div className="flex items-center gap-5">
         {socials.map((social, index) => {
           const Icon = social.Icon;
           return (
             <Link href={social.link} key={index} aria-label={social.label}>
-              <Icon />
+              <Icon className="w-5 h-5 hover:scale-125 transition-all " />
             </Link>
           );
         })}
